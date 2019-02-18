c57822f12fad2b4017a3773e51b2df1cb64e5914
Don Gepulango <dongepulango89@gmail.com>
2019-02-18 20:53:09 +0800

    Updates

---
+++ precache-manifest.08872aae48bf2e36a114eb71655d048a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "df41e76db3fd591f24937e78d1931545",
+    "url": "/react-kit/static/media/LotaGrotesque-Bold.df41e76d.woff"
+  },
+  {
+    "revision": "4d9db46335a6a934a4ed",
+    "url": "/react-kit/static/css/main.094af6ef.chunk.css"
+  },
+  {
+    "revision": "018eaecbf3a8c326b3ed",
+    "url": "/react-kit/static/js/runtime~main.018eaecb.js"
+  },
+  {
+    "revision": "d02f81d53375275d0eb1",
+    "url": "/react-kit/static/js/2.d02f81d5.chunk.js"
+  },
+  {
+    "revision": "f4242690badd63bc66a81bd7eada2eaa",
+    "url": "/react-kit/static/media/BalkanBrothers-logo-white.f4242690.svg"
+  },
+  {
+    "revision": "e7db42b16a76b9b7bb61b3da408107f3",
+    "url": "/react-kit/static/media/LotaGrotesque-Regular.e7db42b1.woff2"
+  },
+  {
+    "revision": "513f6512a122516770454822b3154362",
+    "url": "/react-kit/static/media/LotaGrotesque-Bold.513f6512.woff2"
+  },
+  {
+    "revision": "b34e623453b5a55ad650fcf0fd2bdca0",
+    "url": "/react-kit/static/media/LotaGrotesque-Regular.b34e6234.woff"
+  },
+  {
+    "revision": "4d9db46335a6a934a4ed",
+    "url": "/react-kit/static/js/main.4d9db463.chunk.js"
+  },
+  {
+    "revision": "f144e9469e5174e5d7462b1b655ccb8c",
+    "url": "/react-kit/static/media/LotaGrotesque-Regular.f144e946.ttf"
+  },
+  {
+    "revision": "598e186c2541c3b3cb333636c802f349",
+    "url": "/react-kit/static/media/LotaGrotesque-Regular.598e186c.eot"
+  },
+  {
+    "revision": "102fa106b94efd169324925a5c8a3af1",
+    "url": "/react-kit/static/media/LotaGrotesque-Bold.102fa106.ttf"
+  },
+  {
+    "revision": "b06de3c7ac3a29a6dcd1190e26018d1c",
+    "url": "/react-kit/static/media/LotaGrotesque-Bold.b06de3c7.eot"
+  },
+  {
+    "revision": "c89dd601f23c8e45c535a2e9a680a0f1",
+    "url": "/react-kit/static/media/LotaGrotesque-Regular.c89dd601.svg"
+  },
+  {
+    "revision": "fee8a510e2c181188af44b302baf7926",
+    "url": "/react-kit/static/media/LotaGrotesque-Bold.fee8a510.svg"
+  },
+  {
+    "revision": "f0c4603a29bd80689b76fbbf62826470",
+    "url": "/react-kit/index.html"
+  }
+];
