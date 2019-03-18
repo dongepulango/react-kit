4d07ec5d0b321cb251cd9b22955204c2481d64e9
Don Gepulango <dongepulango89@gmail.com>
2019-03-18 20:11:53 +0800

    Build

---
+++ build/precache-manifest.c8e37d0c49704e59e14a3ee38acfb59d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4242690badd63bc66a81bd7eada2eaa",
+    "url": "/react-kit/static/media/BalkanBrothers-logo-white.f4242690.svg"
+  },
+  {
+    "revision": "018eaecbf3a8c326b3ed",
+    "url": "/react-kit/static/js/runtime~main.018eaecb.js"
+  },
+  {
+    "revision": "a5567d881e9073e1b8d5",
+    "url": "/react-kit/static/js/main.a5567d88.chunk.js"
+  },
+  {
+    "revision": "2528826f290522fc7ba9",
+    "url": "/react-kit/static/js/2.2528826f.chunk.js"
+  },
+  {
+    "revision": "bc56753d1856c433e25ea017245add18",
+    "url": "/react-kit/index.html"
+  }
+];
