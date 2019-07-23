0281935d99a36ecaf905a3ed2c8f826725b9790e
Don Gepulango <dongepulango89@gmail.com>
2019-07-23 17:14:32 +0800

    Updates

---
+++ precache-manifest.625aa2297ed9acaa64708eb658a9c498.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "742ff7252251e2913f3639a120d8f513",
+    "url": "/react-kit/index.html"
+  },
+  {
+    "revision": "16e71c642e4c7915548b",
+    "url": "/react-kit/static/js/2.5e74c844.chunk.js"
+  },
+  {
+    "revision": "56896ffd9d0556dcdea0",
+    "url": "/react-kit/static/js/main.a35e72ea.chunk.js"
+  },
+  {
+    "revision": "bf400cfd1e76f1d985c5",
+    "url": "/react-kit/static/js/runtime~main.9b365e17.js"
+  },
+  {
+    "revision": "f4242690badd63bc66a81bd7eada2eaa",
+    "url": "/react-kit/static/media/BalkanBrothers-logo-white.f4242690.svg"
+  }
+]);
