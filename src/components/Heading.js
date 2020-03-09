30e07e17e024091c2a954285012ca5cd15fbd5b2
Don Gepulango <dongepulango89@gmail.com>
2020-03-10 04:12:00 +0800

    Updates

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,52 +1,41 @@
 //styles
 import styled, { css } from 'styled-components';
-import { between } from 'polished';
+import { between, rem } from 'polished';
 import vars from './Vars';
 
 //styled
 const Heading = styled.h1`
   color: ${vars.colors.textDark};
-  font-weight: 500;
-  line-height: ${props => props.lh ? props.lh : 1.2};
-  margin-bottom: ${props => props.mb ? props.mb : '20px'};
-  font-size: ${props => props.size ? props.size+'px' : '40px'};
-  @media (max-width: ${vars.media.lgMax}) {
-    font-size: ${props => props.size ? between((props.size - 10)+'px', props.size+'px') : between('30px', '40px')};
-  }
+  font-size: ${props => props.fontSize ? rem(props.fontSize + 'px') : rem('40px')};
+  font-weight: ${props => props.fontWeight ? props.fontWeight : 'bold'};
+  line-height: ${props => props.lineHeight ? props.lineHeight : 1.2};
+  margin-bottom: ${props => props.marginBottom ? rem(props.marginBottom + 'px') : rem('20px')};
+  /* @media (max-width: ${vars.media.lgMax}) {
+    font-size: ${props => props.size ? between((props.fontSize - 10) + 'px', props.fontSize + 'px') : between('30px', '40px')};
+  } */
+  /* Heading1 */
+  ${props => props.heading2 && css`
+    font-size: ${props => props.fontSize ? rem(props.fontSize + 'px') : rem('40px')};
+  `}
   /* Heading2 */
   ${props => props.heading2 && css`
-    font-size: ${props => props.size ? props.size+'px' : '32px'};
-    @media (max-width: ${vars.media.lgMax}) {
-      font-size: ${props => props.size ? between((props.size - 8) + 'px', props.size + 'px') : between('24px', '32px')};
-    }
+    font-size: ${props => props.fontSize ? rem(props.fontSize + 'px') : rem('32px')};
   `}
   /* Heading3 */
   ${props => props.heading3 && css`
-    font-size: ${props => props.size ? props.size + 'px' : '28px'};
-    @media (max-width: ${vars.media.lgMax}) {
-      font-size: ${props => props.size ? between((props.size - 8) + 'px', props.size + 'px') : between('22px', '28px')};
-    }
+    font-size: ${props => props.fontSize ? rem(props.fontSize + 'px') : rem('28px')};
   `}
   /* Heading4 */
   ${props => props.heading4 && css`
-    font-size: ${props => props.size ? props.size + 'px' : '24px'};
-    @media (max-width: ${vars.media.lgMax}) {
-      font-size: ${props => props.size ? between((props.size - 6) + 'px', props.size + 'px') : between('18px', '24px')};
-    }
+    font-size: ${props => props.fontSize ? rem(props.fontSize + 'px') : rem('24px')};
   `}
   /* Heading5 */
   ${props => props.heading5 && css`
-    font-size: ${props => props.size ? props.size + 'px' : '20px'};
-    @media (max-width: ${vars.media.lgMax}) {
-      font-size: ${props => props.size ? between((props.size - 4) + 'px', props.size + 'px') : between('16px', '20px')};
-    }
+    font-size: ${props => props.fontSize ? rem(props.fontSize + 'px') : rem('20px')};
   `}
   /* Heading6 */
   ${props => props.heading6 && css`
-    font-size: ${props => props.size ? props.size + 'px' : '16px'};
-    @media (max-width: ${vars.media.lgMax}) {
-      font-size: ${props => props.size ? props.size + 'px' : '16px'};
-    }
+    font-size: ${props => props.fontSize ? rem(props.fontSize + 'px') : rem('16px')};
   `}
 `;
 
