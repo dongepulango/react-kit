b08e440a85a3d644d94e3b9a9d1bce9b047321ff
Don Gepulango <dongepulango89@gmail.com>
2020-03-25 20:09:57 +0800

    Updates

---
+++ precache-manifest.fde0b87dadcbab7b582bb78019258d63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc097de28c3c9a22cd15276a390cb912",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ace386e4cbc7cd223358",
+    "url": "/static/js/2.f42285f6.chunk.js"
+  },
+  {
+    "revision": "c27e167d6e2cc1d9eddf7fde2be56a6c",
+    "url": "/static/js/2.f42285f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b2ee27a494c3b6b6ace",
+    "url": "/static/js/main.897d93c1.chunk.js"
+  },
+  {
+    "revision": "860e7dae6d9531e0bdfc",
+    "url": "/static/js/runtime-main.e5f4cb60.js"
+  },
+  {
+    "revision": "f4242690badd63bc66a81bd7eada2eaa",
+    "url": "/static/media/BalkanBrothers-logo-white.f4242690.svg"
+  }
+]);
